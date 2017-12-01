4cc807ebc5f5d3e02da199eaa8da9f89fc2a8acb
zhaoning <zhaoning801@163.com>
2017-12-01 09:46:52 +0800

    OCR test

--- tesseract_ocr/ocr.py
+++ tesseract_ocr/ocr.py
@@ -1,10 +1,8 @@
 # coding=utf-8
+
 import subprocess
+
 filename = "paixu.png"
 subprocess.call(['tesseract','-l','chi_sim',filename,'paixu'])
-
 with open('paixu.txt','r') as f:
     print f.read()
-
-
-
